9d7c727929ff04fb568ba2cfce442c9b6591cc6a
yanyan155 <jm@tut.by>
2018-10-28 14:30:54 +0300

    feat: add make function

---
+++ src/make.js
@@ -0,0 +1,20 @@
+function make(...args) {
+  var store = [...args];
+  var res;
+  
+  function recursion(...elemns) {
+
+    var isNan = isNaN(elemns[0]);
+
+    if (!isNan) {
+      store = [...store, ...elemns];
+      return recursion;
+    } else {
+      var fn = elemns[0];
+      var res = store.reduce((prev, curr) => fn(prev, curr));
+      return res;
+    }
+  }
+
+  return recursion;
+}
