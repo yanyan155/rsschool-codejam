e021d145e66d4cc3d832ff333117cba5fb6be064
yanyan155 <jm@tut.by>
2018-10-28 14:33:54 +0300

    feat: add recursion function

---
+++ src/recursion.js
@@ -0,0 +1,33 @@
+function recursion(tree) {
+
+  var nodes = [];
+  var ordered = [];
+
+  function going(tree,level) {
+    level++;
+    var obj = {
+      value: tree.value,
+      level: level
+    }
+    nodes.push(obj);
+    if(tree.left) {
+      going(tree.left, level);
+    }
+    if(tree.right) {
+      going(tree.right, level);
+    }
+  }
+
+  going(tree, 0);
+
+  nodes.sort((a,b) => b.level - a.level);
+
+  for(var i = 1; i<=nodes[0].level; i++) {
+
+    var filter = nodes.filter(elem => elem.level === i);
+    var toPush = filter.map(elem => elem.value);
+    ordered.push(toPush);
+  }
+
+  return ordered;
+}
